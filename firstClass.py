c78b76e400dd6b837241027b68f1673588630abd
gabi <annagabsoares@gmail.com>
2021-07-13 16:12:31 -0700

    first commit

---
+++ firstClass.py
@@ -0,0 +1,16 @@
+# Variable and IO (input/ooutput)
+
+car = "lexus" 
+
+# input
+
+anotherCar = input("What is your car name?")
+
+print(f"cool! My favorite car is {car} and yours is {anotherCar}")
+
+
+#CONSTANTS  - a type of variable whose CANNOT be changed. - containers values that cannot be reassigned.
+#it is nor reinforced by Python.It is more like a style
+GRAVITY = 9.8
+GRAVITY = input(f"gravity value:")
+print(f'gravity is {GRAVITY}')
