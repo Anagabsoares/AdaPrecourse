2bea731e9777ee1700b0f2ba59850a3474e393ee
gabi <annagabsoares@gmail.com>
2021-07-19 13:10:29 -0700

    changes

--- game.py
+++ game.py
@@ -1,9 +1,10 @@
 import random
 
-RANGE_LOW = 0
+RANGE_LOW = 1
 RANGE_HIGH = 100
+MAX_GUESSES = 4
 
-random_numer = random.randint(RANGE_LOW, RANGE_HIGH)
+random_number = random.randint(RANGE_LOW, RANGE_HIGH)
 
 
 
@@ -19,8 +20,32 @@ def get_number_from_user():
             valid_input = True #change the flag variable value when conditional is met
         else:
             print("You must input a number!")
-    print(user_input)
+    print(user_input)       
     return user_input
 
 
-get_number_from_user()                
+def guessed_number_right():
+    random_number = random.randint(RANGE_LOW, RANGE_HIGH)
+    print(random_number)
+    guessed_right = False
+    num_guesses = 0
+    while not guessed_right and num_guesses < MAX_GUESSES:
+        user_input = get_number_from_user()
+        num_guesses+=1
+        print(num_guesses)
+        if user_input < RANGE_LOW or user_input > RANGE_HIGH:
+            print(f'your guess  is out of bounds.')
+            print(f'It must be between {RANGE_LOW} and {RANGE_HIGH}')
+        elif user_input == random_number:
+            print("You guessed the number!  Good job!")
+            guessed_right = True
+        elif user_input > random_number:
+            print("Your guess is too high")   
+        elif user_input < random_number:
+            print("Your guess is too low") 
+    if num_guesses == MAX_GUESSES and not guessed_right:
+        print('YOU EXCESSED THE GUESSES LIMIT')     
+    return f' The number is {random_number}'
+
+
+guessed_number_right()
--- loops.py
+++ loops.py
@@ -79,3 +79,25 @@ def print_multiple(word, amount):
             final_value += " "
         final_value += f'{item} {word}'
     return final_value
+
+
+def guess_the_number( value ):
+    number_guessed = input('What is the number guessed?')
+    if number_guessed.isnumeric():
+        user_input_integer = int(number_guessed)
+        print(number_guessed)
+        if user_input_integer == value:
+            print(f'You won, the misterious value is {value}')  
+            return  
+        if user_input_integer > value:
+            print("The number entered is to high")
+            return
+        else:
+            print('The number entered is to low ')
+            return    
+    else: 
+        while not number_guessed.isnumeric():
+            print("Enter a numberrrr")
+            user_input= input("Guessss the number: ") 
+            if user_input.isnumeric():
+                return guess_the_number(user_input)    
